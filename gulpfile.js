2ffc21189840a9a4e05eaba78ee2019dd307ed3b
ChongoShaun <chongo@gmail.com>
2015-06-09 15:21:45 -0500

    Updated Readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,82 @@
-var gulp = require('gulp'), 
-    sass = require('gulp-ruby-sass') 
-    notify = require("gulp-notify") 
-    bower = require('gulp-bower');
+var gulp = require('gulp'); 
+
+// Plugins
+var browserSync = require('browser-sync').create();
+var bower = require('gulp-bower');
+var notify = require("gulp-notify");
+var jshint = require('gulp-jshint');
+var sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var reload = browserSync.reload;
 
 var config = {
-     sassPath: './resources/sass',
-     bowerDir: './bower_components' 
+    sassPath: './resources/sass',
+    bowerDir: './bower_components'
 }
 
-gulp.task('bower', function() { 
+gulp.task('bower', function() {
     return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
+        .pipe(gulp.dest(config.bowerDir))
 });
 
-gulp.task('icons', function() { 
-    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest('./public/fonts')); 
+gulp.task('lint', function() {
+    return gulp.src('./public/assets/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
-gulp.task('css', function() { 
-    return gulp.src(config.sassPath + '/style.scss')
-         .pipe(sass({
-             style: 'compressed',
-             loadPath: [
-                 './resources/sass',
-                 config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-             ]
-         }) 
+gulp.task('scripts', function() {
+    return gulp.src('./public/assets/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/assets/dist'));
+});
+
+
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('./public/assets/fonts'));
+});
+
+gulp.task('css', function() {
+    return gulp.src(config.sassPath + '/sdms_style.scss')
+        .pipe(sass({
+            style: 'compressed',
+            loadPath: [
+                './resources/sass',
+                config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+                config.bowerDir + '/fontawesome/scss',
+            ]
+        })
             .on("error", notify.onError(function (error) {
-                 return "Error: " + error.message;
-             }))) 
-         .pipe(gulp.dest('./public/css')); 
+                return "Error: " + error.message;
+            })))
+        .pipe(gulp.dest('./public/assets/css'))
+        .pipe(reload({stream: true}));
+
 });
 
 // Rerun the task when a file changes
- gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+
+gulp.task('watch', function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-  gulp.task('default', ['bower', 'icons', 'css']);
+// Watch Files For Changes
+gulp.task('watch', function() {
+   browserSync.init({
+        proxy: "dev.sdms.10.1.10.13.xip.io:8888",
+        host: "localhost",
+        port: 8888
+    });
+    gulp.watch('./public/assets/js/*.js', ['lint', 'scripts']);
+    gulp.watch([config.sassPath + '/**/*.scss', './public/assets/scss/*.scss'], ['css']);
+    gulp.watch("./*.html").on('change', reload);
+
+});
+
+gulp.task('default', ['bower', 'icons', 'css', 'lint', 'scripts', 'watch']);
